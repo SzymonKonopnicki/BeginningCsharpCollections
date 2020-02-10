96a9321c09ec6ce04299b468068a6ef599ca97d3
mariuszjurczenko <mariuszjurczenko@gmail.com>
2020-02-10 11:02:21 +0100

    2.6) Parsing a CSV Line.

--- 2 ArrayPracticalExample/CsvReader.cs
+++ 2 ArrayPracticalExample/CsvReader.cs
@@ -11,7 +11,22 @@
 
         public City[] ReadFirstNCiyts(int nCitys)
         {
-            return null;
+            City[] citys = new City[nCitys];
+            return citys;
+        }
+
+        public City ReadCityFromCsvLine(string csvLine)
+        {
+            string[] parts = csvLine.Split(',');
+
+            string cityName = parts[0];
+            string cityCode = parts[1];
+            string country = parts[2];
+            int totalPopulation = int.Parse(parts[3]);
+            int menPopulation = int.Parse(parts[4]);
+            int womenPopulation = int.Parse(parts[5]);
+
+            return new City(cityName, cityCode, country, totalPopulation, menPopulation, womenPopulation);
         }
     }
 }
