774c51d57fd3f852b9f44d8a59e5232ac1a99a9c
mariuszjurczenko <mariuszjurczenko@gmail.com>
2020-02-10 08:49:44 +0100

    2.2) Importing data from file.

---
+++ 2 ArrayPracticalExample/CsvReader.cs
@@ -0,0 +1,17 @@
+﻿namespace _2_ArrayPracticalExample
+{
+    public class CsvReader
+    {
+        private string _csvFilePath;
+
+        public CsvReader(string csvFilePath)
+        {
+            _csvFilePath = csvFilePath;
+        }
+
+        public City[] ReadFirstNCiyts(int nCitys)
+        {
+            return null;
+        }
+    }
+}
--- 2 ArrayPracticalExample/Program.cs
+++ 2 ArrayPracticalExample/Program.cs
@@ -6,7 +6,16 @@ namespace _2_ArrayPracticalExample
     {
         static void Main(string[] args)
         {
+            string filePath = @"C:\Users\user\source\repos\BeginningCsharpCollections\2 ArrayPracticalExample\population.csv";
 
+            CsvReader csvReader = new CsvReader(filePath);
+
+            City[] citys = csvReader.ReadFirstNCiyts(7);
+
+            foreach (var city in citys)
+            {
+                Console.WriteLine($"{city.TotalPopulation} : {city.CityCode} : {city.CityName}");
+            }
         }
     }
 }
