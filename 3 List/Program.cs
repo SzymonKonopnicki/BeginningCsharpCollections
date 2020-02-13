56f6f786608c5d9d1bc87a1df2acf80acf3a7dd8
mariuszjurczenko <mariuszjurczenko@gmail.com>
2020-02-13 10:55:47 +0100

    3.4) Adding Items.

--- 3 List/Program.cs
+++ 3 List/Program.cs
@@ -6,13 +6,27 @@ namespace _3_List
     class Program
     {
         static void Main(string[] args)
-        {
-            List<string> months = new List<string>();
-
+        {           
             List<int> ints = new List<int>();
 
             List<char> chars = new List<char>();
 
+
+            List<string> months = new List<string>
+            {
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December"
+            };
 
         }
     }
