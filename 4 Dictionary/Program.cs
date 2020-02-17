b3dc5e981a4de833edc2f69b984ed64ccc69e7ca
mariuszjurczenko <mariuszjurczenko@gmail.com>
2020-02-17 09:47:32 +0100

    Add Project 4_Dictionary

---
+++ 4 Dictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4_Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\user\source\repos\BeginningCsharpCollections\2 ArrayPracticalExample\population.csv";
+
+            CsvReader csvReader = new CsvReader(filePath);
+
+            List<City> citys = csvReader.ReadAllCiyts();
+
+            City cityTest = new City("Test", "TES", "Polska", 3000000, 1600000, 1400000);
+
+            int index = citys.FindIndex(city => city.TotalPopulation < 3000000);
+
+            foreach (var city in citys)
+            {
+                Console.WriteLine($"{city.TotalPopulation:### ### ###} : {city.CityCode} : {city.CityName}");
+            }
+
+            Console.WriteLine($"{citys.Count} citys");
+        }
+    }
+}
