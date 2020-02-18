f08a73e092fd952e2a7960af14ba1f4464c7bc89
mariuszjurczenko <mariuszjurczenko@gmail.com>
2020-02-18 12:20:02 +0100

    4.9) Searching for items.

--- 4 Dictionary/Program.cs
+++ 4 Dictionary/Program.cs
@@ -10,22 +10,27 @@ namespace _4_Dictionary
             City slaskie = new City("Slaskie", "SLA", "Polska", 4524091, 2180662, 2343429);
             City malopolskie = new City("Malopolskie", "MLP", "Polska", 3404863, 1652663, 1752200);
 
-            //List<City> citys = new List<City>();
-            //citys.Add(slaskie);
-            //citys.Add(slaskie);
-
-            //foreach (City city in citys)
-            //{
-            //    Console.WriteLine(city.CityName);
-            //}
-
             var citys = new Dictionary<string, City>();
             citys.Add(slaskie.CityCode, slaskie);
-            citys.Add(slaskie.CityCode, slaskie);
+            citys.Add(malopolskie.CityCode, malopolskie);
 
-            foreach (var city in citys.Values)
+            var searchedCity = citys.TryGetValue("WLK", out City city);
+            
+            if (searchedCity)
+                Console.WriteLine(city.CityName);
+            else
+                Console.WriteLine("There is no city with this code");
+
+
+            citys.Remove(malopolskie.CityCode);
+            citys[slaskie.CityCode] = malopolskie;
+
+            bool exist = citys.ContainsKey(slaskie.CityCode);
+            Console.WriteLine(exist);
+
+            foreach (var city2 in citys.Values)
             {
-                Console.WriteLine(city);
+                Console.WriteLine(city2.CityName);
             }
 
         }
