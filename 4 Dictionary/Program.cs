268f24dabc9c76f0cf8c5e85b71ea400d4ffb0b7
mariuszjurczenko <mariuszjurczenko@gmail.com>
2020-02-18 11:41:12 +0100

    4.8) Keys Are Unique!

--- 4 Dictionary/Program.cs
+++ 4 Dictionary/Program.cs
@@ -10,11 +10,18 @@ namespace _4_Dictionary
             City slaskie = new City("Slaskie", "SLA", "Polska", 4524091, 2180662, 2343429);
             City malopolskie = new City("Malopolskie", "MLP", "Polska", 3404863, 1652663, 1752200);
 
-            var citys = new Dictionary<string, City>
-            {
-                { slaskie.CityCode, slaskie },
-                { malopolskie.CityCode, malopolskie }
-            };
+            //List<City> citys = new List<City>();
+            //citys.Add(slaskie);
+            //citys.Add(slaskie);
+
+            //foreach (City city in citys)
+            //{
+            //    Console.WriteLine(city.CityName);
+            //}
+
+            var citys = new Dictionary<string, City>();
+            citys.Add(slaskie.CityCode, slaskie);
+            citys.Add(slaskie.CityCode, slaskie);
 
             foreach (var city in citys.Values)
             {
