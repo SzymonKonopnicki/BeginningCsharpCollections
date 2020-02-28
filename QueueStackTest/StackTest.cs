be2cfe4c785a3bd52d6b9b266dfe9c618176e4f7
mariuszjurczenko <mariuszjurczenko@gmail.com>
2020-02-28 10:21:56 +0100

    5.14) ToArray().

--- QueueStackTest/StackTest.cs
+++ QueueStackTest/StackTest.cs
@@ -35,5 +35,25 @@ namespace QueueStackTest
 
             Assert.IsTrue(stack.Contains(5));
         }
+
+        [TestMethod]
+        public void StackUseToArray()
+        {
+            var stack = new Stack<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            stack.Push(6);
+
+            var array = stack.ToArray();
+            stack.Pop();
+
+            Assert.AreEqual(6, array[0]);
+            Assert.AreEqual(6, array.Length);
+            Assert.AreEqual(5, stack.Count);
+        }
     }
 }
