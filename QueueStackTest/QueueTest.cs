1bb7fc61f2ef44f0bb7b4f7f969f119f4d97c23e
mariuszjurczenko <mariuszjurczenko@gmail.com>
2020-02-28 09:36:38 +0100

    5.8) Clear().

--- QueueStackTest/QueueTest.cs
+++ QueueStackTest/QueueTest.cs
@@ -55,5 +55,21 @@ namespace QueueStackTest
             Assert.AreEqual(6, array.Length);
             Assert.AreEqual(5, queue.Count);
         }
+
+        [TestMethod]
+        public void QueueUseClear()
+        {
+            var queue = new Queue<int>();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            queue.Enqueue(6);
+            queue.Clear();
+
+            Assert.AreEqual(0, queue.Count);
+        }
     }
 }
