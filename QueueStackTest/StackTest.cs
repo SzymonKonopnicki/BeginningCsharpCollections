44a36de3009454c114bb2a672d741ea6021b6968
mariuszjurczenko <mariuszjurczenko@gmail.com>
2020-02-28 10:16:01 +0100

    5.13) Contains().

--- QueueStackTest/StackTest.cs
+++ QueueStackTest/StackTest.cs
@@ -20,5 +20,20 @@ namespace QueueStackTest
 
             Assert.AreEqual(6, stack.Peek());
         }
+
+        [TestMethod]
+        public void StackUseContains()
+        {
+            var stack = new Stack<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            stack.Push(6);
+
+            Assert.IsTrue(stack.Contains(5));
+        }
     }
 }
