81b889824739b7df08cdf3774ec4b3f49a5e9bc3
mariuszjurczenko <mariuszjurczenko@gmail.com>
2020-02-28 09:20:52 +0100

    5.6) Contains().

--- QueueStackTest/QueueTest.cs
+++ QueueStackTest/QueueTest.cs
@@ -20,5 +20,20 @@ namespace QueueStackTest
 
             Assert.AreEqual(1, queue.Peek());
         }
+
+        [TestMethod]
+        public void QueueUseContains()
+        {
+            var queue = new Queue<int>();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            queue.Enqueue(6);
+
+            Assert.IsTrue(queue.Contains(3));
+        }
     }
 }
