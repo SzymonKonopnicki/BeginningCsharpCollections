77f51b149b9a12f85e2dc3a4df1dc1ae8a861ada
mariuszjurczenko <mariuszjurczenko@gmail.com>
2020-02-28 09:30:14 +0100

    5.7) ToArray().

--- QueueStackTest/QueueTest.cs
+++ QueueStackTest/QueueTest.cs
@@ -35,5 +35,25 @@ namespace QueueStackTest
 
             Assert.IsTrue(queue.Contains(3));
         }
+
+        [TestMethod]
+        public void QueueUseToArray()
+        {
+            var queue = new Queue<int>();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            queue.Enqueue(6);
+
+            var array = queue.ToArray();
+            queue.Dequeue();
+
+            Assert.AreEqual(1, array[0]);
+            Assert.AreEqual(6, array.Length);
+            Assert.AreEqual(5, queue.Count);
+        }
     }
 }
