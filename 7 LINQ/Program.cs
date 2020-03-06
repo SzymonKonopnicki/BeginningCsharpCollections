c7132bdfc2d0a16347ea7898788358636fdf1f2f
mariuszjurczenko <mariuszjurczenko@gmail.com>
2020-03-06 09:54:31 +0100

    7.6) Other operations.

--- 7 LINQ/Program.cs
+++ 7 LINQ/Program.cs
@@ -12,15 +12,25 @@ namespace _7_LINQ
 
             CsvReader csvReader = new CsvReader(filePath);
 
-            List<City> citys = csvReader.ReadAllCiyts();          
+            List<City> citys = csvReader.ReadAllCiyts();
 
-            foreach (var city in citys.Where(c => !c.CityName.Contains("Test")).Take(10))
-            {
-                Console.WriteLine($"{city.TotalPopulation:### ### ###} : {city.CityCode} : {city.CityName}");
-            }
+            //foreach (var city in citys.Where(c => !c.CityName.Contains("Test")).Take(10))
+            //{
+            //    Console.WriteLine($"{city.TotalPopulation:### ### ###} : {city.CityCode} : {city.CityName}");
+            //}
 
-            for (int i = 3; i <= 8; i++)
-                Console.WriteLine(citys[i].CityName);
+            //for (int i = 3; i <= 8; i++)
+            //    Console.WriteLine(citys[i].CityName);
+
+
+            Console.WriteLine($"{citys.Max(c => c.MenPopulation):### ### ###} - The city with the largest male population");
+            Console.WriteLine($"{citys.Min(c => c.WomenPopulation):### ### ###} - The city with the smallest female population");
+            Console.WriteLine($"{citys.Sum(c => c.TotalPopulation):### ### ###} - Total population of all cities");
+
+            Console.WriteLine($"{citys.Average(c => c.MenPopulation):### ### ###} - Average male population");
+
+            Console.WriteLine(citys.All(c => c.TotalPopulation > 200000));
+            Console.WriteLine(citys.Any(c => c.TotalPopulation > 20000000));
         }
     }
 }
