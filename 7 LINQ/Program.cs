ec116e0aa9da75335e6c63929ae1ee99a2d5e4f9
mariuszjurczenko <mariuszjurczenko@gmail.com>
2020-03-06 08:14:21 +0100

    Add Project 7 LINQ.

---
+++ 7 LINQ/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\user\source\repos\BeginningCsharpCollections\2 ArrayPracticalExample\population.csv";
+
+            CsvReader csvReader = new CsvReader(filePath);
+
+            List<City> citys = csvReader.ReadAllCiyts();
+
+            csvReader.RemoveTestCitys(citys);
+
+            foreach (var city in citys)
+            {
+                Console.WriteLine($"{city.TotalPopulation:### ### ###} : {city.CityCode} : {city.CityName}");
+            }
+        }
+    }
+}
