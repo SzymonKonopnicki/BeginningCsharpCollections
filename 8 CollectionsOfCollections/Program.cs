ed59d38f76eb14ed5c4ba3adb649ef0552167f1b
mariuszjurczenko <mariuszjurczenko@gmail.com>
2020-03-10 09:21:51 +0100

    Add Project 8 Collections Of Collections

---
+++ 8 CollectionsOfCollections/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8_CollectionsOfCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Users\user\source\repos\BeginningCsharpCollections\8 CollectionsOfCollections\population.csv";
+
+            CsvReader csvReader = new CsvReader(filePath);
+
+            List<City> citys = csvReader.ReadAllCiyts();
+
+            foreach (var city in citys)
+            {
+                Console.WriteLine($"{city.TotalPopulation:### ### ###} : {city.CityCode} : {city.CityName}");
+            }
+        }
+    }
+}
