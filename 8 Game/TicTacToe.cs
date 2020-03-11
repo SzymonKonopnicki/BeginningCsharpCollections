d4cbf85e0b3b8fc65529c48556c2fa6fca8d0ea7
mariuszjurczenko <mariuszjurczenko@gmail.com>
2020-03-11 10:15:15 +0100

    8.9) TicTacToe part.4

--- 8 Game/TicTacToe.cs
+++ 8 Game/TicTacToe.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace _8_Game
 {
@@ -37,11 +35,37 @@ namespace _8_Game
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    Console.WriteLine(" " + board[i][j]);
+                    Console.Write(" " + board[i][j]);
                 }
 
                 Console.WriteLine();
             }
+        }
+
+        private bool PlayMove(Player player)
+        {
+            Console.WriteLine("Invalid input quits game");
+            Console.WriteLine($"{player}: Enter coordinates eg. 2,2 >");
+            string input = Console.ReadLine();
+            string[] coordinates = input.Split(',');
+
+            if (coordinates.Length != 2)
+                return false;
+
+            int.TryParse(coordinates[0], out int row);
+            int.TryParse(coordinates[1], out int column);
+
+            if (row < 1 || row > 3 || column < 1 || column > 3)
+                return false;
+
+            if (board[row - 1][column - 1].Owner != Player.None)
+            {
+                Console.WriteLine("Board occupied !");
+                return false;
+            }
+
+            board[row - 1][column - 1] = new Square(player);
+            return true;
         }
     }
 }
