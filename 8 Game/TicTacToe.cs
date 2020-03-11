1649c54ccf3785deb3004abf87623ee4988d278e
mariuszjurczenko <mariuszjurczenko@gmail.com>
2020-03-11 09:21:54 +0100

    8.6) TicTacToe.

---
+++ 8 Game/TicTacToe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _8_Game
+{
+    public class TicTacToe
+    {
+        private Square[][] board =
+        {
+            new Square[3],
+            new Square[3],
+            new Square[3]
+        };
+    }
+}
